e2c9877cc822d73f47b3d0e2fe5ec30e9c041ea5
Margarita Golubeva <margaret.golubeva@mail.ru>
2023-12-16 18:32:52 +0300

    refactor: replace for loop with map method in deleteDigit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -12,17 +12,11 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function deleteDigit(n) {
-  let str = n.toString();
-  let result = "";
-  let arr = [];
-  let highest = 0;
-
-  for (let i = 0; i < str.length; i++) {
-    result = removeCharAt(str, i);
-    arr.push(+result);
-    highest = Math.max(highest, +result);
-  }
-  return highest;
+  const str = n.toString();
+  // 1. split string
+  // 2. .map - create new array by iterating and replacing (instead of for loop)
+  // 3. find maximum value
+  return Math.max(...str.split('').map((_, i) => +removeCharAt(str, i)));
 }
 
 function removeCharAt(str, i) {
