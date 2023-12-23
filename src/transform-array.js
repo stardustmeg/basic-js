4a6c554ef08fc003462743d8acfae17e0adc727e
Margarita Golubeva <margaret.golubeva@gmail.com>
2023-12-24 02:31:49 +0300

    feat: solve Transform Array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,61 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const transformedArr = [];
+
+  const doubleNext = "--double-next";
+  const discardPrev = "--discard-prev";
+  const discardNext = "--discard-next";
+  const doublePrev = "--double-prev";
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case doubleNext:
+        if (i < arr.length - 1) {
+          transformedArr.push(arr[i + 1]);
+        }
+        break;
+
+      case discardPrev:
+        if (i > 0 && arr[i - 2] !== discardNext) {
+          transformedArr.pop();
+        }
+        break;
+
+      case discardNext:
+        i++;
+        break;
+
+      case doublePrev:
+        if (i > 0 && arr[i - 2] !== discardNext) {
+          transformedArr.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        transformedArr.push(arr[i]);
+    }
+  }
+  return transformedArr;
 }
 
 module.exports = {
-  transform
+  transform,
 };
