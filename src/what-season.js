1d738c15da27c7bd7984d53e14f0aaf370f1a3e0
Margarita Golubeva <margaret.golubeva@gmail.com>
2023-12-23 22:49:35 +0300

    feat: solve base rqmts getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,11 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+
+  const month = date.getMonth();
+
+  if ((month >= 0 && month <= 1) || month === 11) {
+    return "winter";
+  } else if (month >= 2 && month <= 4) {
+    return "spring";
+  } else if (month >= 5 && month <= 7) {
+    return "summer";
+  } else {
+    return "fall";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
